f7090122d8439e463d2b131b6a59a3fb786b28f0
JoseCprietsch <85766830+JoseCprietsch@users.noreply.github.com>
2026-07-14 09:33:13 -0300

    Update photos-data.js

--- photos-data.js
+++ photos-data.js
@@ -1,15 +1,25 @@
 // ===== FOTOS DO CASAL (carrossel 3D) =====
 // Cole aqui as URLs das fotos (do GitHub, ou de qualquer link direto de imagem).
 // Pode ter quantas quiser -- o carrossel se ajusta sozinho.
-// OBS: por enquanto está com 8 cópias da mesma foto, só pra visualizar o efeito.
-// Troque por fotos diferentes quando tiver.
 const WC_PHOTOS = [
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
-  'JuliaeJefferson.png',
+  'IMG-20260714-WA0000.jpg',
+  'IMG-20260714-WA0001.jpg',
+  'IMG-20260714-WA0002.jpg',
+  'IMG-20260714-WA0003.jpg',
+  'IMG-20260714-WA0004.jpg',
+  'IMG-20260714-WA0005.jpg',
+  'IMG-20260714-WA0006.jpg',
+  'IMG-20260714-WA0007.jpg',
+  'IMG-20260714-WA0008.jpg',
+  'IMG-20260714-WA0009.jpg',
+  'IMG-20260714-WA0010.jpg',
+  'IMG-20260714-WA0011.jpg',
+  'IMG-20260714-WA0012.jpg',
+  'IMG-20260714-WA0013.jpg',
+  'IMG-20260714-WA0014.jpg',
+  'IMG-20260714-WA0015.jpg',
+  'IMG-20260714-WA0016.jpg',
+  'IMG-20260714-WA0017.jpg',
+  'IMG-20260714-WA0018.jpg',
+  'IMG-20260714-WA0019.jpg',
 ];
